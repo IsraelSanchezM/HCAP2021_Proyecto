4f7492235d7ef218b6316946be9e2de5ae8fc3c0
IsraelSanchezM <A01378705@itesm.mx>
2021-03-18 11:33:02 -0600

    Documentación del programa de convolución

--- convolucion.py
+++ convolucion.py
@@ -1,8 +1,30 @@
+'''
+Programa que cuenta con la función de convolución de una imágen y que posteriormente realiza un ejemplo de convolución
+Autores: 
+    * David Rodríguez Fragoso A01748760
+    * Israel Sánchez Miranda A01378705
+    * José Miguel García Gurtubay Moreno A01373750
+    * Omar Rodrigo Sorchini Puente A01749389
+    * Paola Dorantes Calderón A01653108
+18/03/2021
+'''
+
 #Bibliotecas importadas
-import numpy as np
+import numpy as np #Numpy
 
 #Función de convolución
 def convolucion(IOriginal, Kernel):
+    '''
+    Función que se encarga de realizar la convolución a una imágen con un Kernel
+
+    Parámetros:
+    * IOriginal = Matriz de la imagen original a la que se le aplicará la convolución
+    * Kernel = Matriz que servirá como el Kernel de la convolución de la imagen
+
+    Retornos:
+    * Res = Matriz resultante de realizar la convolución de la imagen
+    '''
+
     #Variables:
     fr = len(IOriginal) - (len(Kernel) - 1)           #Número de filas de la matriz resultante
     cr = len(IOriginal[0]) - (len(Kernel[0]) - 1)     #Número de columnas de la matriz resultante 
@@ -23,7 +45,7 @@ def convolucion(IOriginal, Kernel):
 
 #Probando la función
 #Variables
-K = [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]]                                                   #Kernel
+K = [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]]                                                    #Kernel
 I = [[2, 0, 1, 1, 1], [3, 0, 0, 0, 2], [1, 1, 1, 1, 1], [3, 1, 1, 1, 2], [1, 1, 1, 1, 1]]   #Imágen
 In = np.array(I)  #Array de la imágen en numpy
 Kn = np.array(K)  #Array del Kernel en numpy
