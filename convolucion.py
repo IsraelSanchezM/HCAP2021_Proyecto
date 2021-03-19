6a1e8eb79c129eeb94930590df21f22e41339262
IsraelSanchezM <A01378705@itesm.mx>
2021-03-19 09:12:47 -0600

    Nuevos cambios al código de convolución

--- convolucion.py
+++ convolucion.py
@@ -28,7 +28,7 @@ def convolucion(IOriginal, Kernel):
     #Variables:
     fr = len(IOriginal) - (len(Kernel) - 1)           #Número de filas de la matriz resultante
     cr = len(IOriginal[0]) - (len(Kernel[0]) - 1)     #Número de columnas de la matriz resultante 
-    Res = np.zeros((fr, cr))                          #Matriz resultante
+    Res = np.zeros((fr, cr), np.uint8)                          #Matriz resultante
 
     #For para recorrer las filas
     for i in range(len(Res)):
@@ -40,9 +40,12 @@ def convolucion(IOriginal, Kernel):
                 #For para recorrer las columnas del Kernel
                 for n in range(len(Kernel[0])):
                     suma += Kernel[m][n] * IOriginal[m + i][n + j] #Multiplicación de matrices
-            Res[i][j] = suma #Sustituyendo el valor de la multiplicación en su celda correspondiente
+            if suma <= 255:
+                Res[i][j] = round(suma) #Sustituyendo el valor de la multiplicación en su celda correspondiente
+            else:
+                Res[i][j] = 255
     return Res #Se regresa la matriz resultante
-
+'''
 #Probando la función
 #Variables
 K = [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]]                                                    #Kernel
@@ -51,4 +54,5 @@ In = np.array(I)  #Array de la imágen en numpy
 Kn = np.array(K)  #Array del Kernel en numpy
 
 R = convolucion(In, Kn) #Se hace la matriz resultante mandando a llamar la función de convolución
-print(R)   #Se imprime la matriz resultante de la convoluciór
+print(R)   #Se imprime la matriz resultante de la convolución
+'''
