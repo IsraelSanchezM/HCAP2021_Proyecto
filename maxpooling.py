2b3c890b9c9099f2d2a47ba8e88d10789a327e44
IsraelSanchezM <A01378705@itesm.mx>
2021-03-19 10:02:28 -0600

    Pipeline con la convolución y el max pooling

--- maxpooling.py
+++ maxpooling.py
@@ -14,9 +14,10 @@ def maxpooling(Img):
             j += 1
         i += 1        
     return Res
-
+'''
 I = [[1, 2, 3, 4], [5, 6, 7, 8], [4, 3, 2, 1], [1, 3, 7, 9]]
 IA = np.array(I)
 print(IA)
 IR = maxpooling(IA)
 print(IR)
+'''
